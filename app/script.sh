371431bbaf02a080abd6457f8bb6e91cabee957c
eloco <exia@isimarkets.com>
2022-06-08 14:29:24 +0800

    modified:   script.sh

--- app/script.sh
+++ app/script.sh
@@ -6,6 +6,7 @@ if [ $PROXY ];then
 	echo "PROXY = $PROXY"
     awk -v find="#proxy:" -v repl="proxy: ${PROXY}" '{ while (i=index($0,find))  $0 = substr($0,1,i-1) repl substr($0,i+length(find)) ; print }'  dcompass.yaml > dcompass.yaml.bak
     mv dcompass.yaml.bak dcompass.yaml 
+    cat dcompass.yaml
 else
     echo 'PROXY not set'
 fi
